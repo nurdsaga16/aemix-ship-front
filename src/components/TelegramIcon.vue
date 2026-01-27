7aa21496a28774823daae0c65c7dad2ee825242a
nurdsaga16 <nurd.saga14@gmail.com>
2026-01-27 17:15:44 +0500

    changed telegram icon

--- src/components/TelegramIcon.vue
+++ src/components/TelegramIcon.vue
@@ -1,17 +1,13 @@
 <template>
   <svg
     class="w-5 h-5"
-    viewBox="0 0 24 24"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
+    x="0px" y="0px"
+    viewBox="0,0,256,256"
   >
-    <path
-      d="M9.75 15.75L9.5 19L12 17.25L14.5 19L14.25 15.75L18.5 12.25L19.25 8.75L4.75 13.75L8.25 15L16.75 8.75"
-      stroke="currentColor"
-      stroke-width="1.6"
-      stroke-linecap="round"
-      stroke-linejoin="round"
-    />
+  <g fill="#ffffff" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" style="mix-blend-mode: normal"><g transform="scale(5.12,5.12)"><path d="M46.137,6.552c-0.75,-0.636 -1.928,-0.727 -3.146,-0.238h-0.002c-1.281,0.514 -36.261,15.518 -37.685,16.131c-0.259,0.09 -2.521,0.934 -2.288,2.814c0.208,1.695 2.026,2.397 2.248,2.478l8.893,3.045c0.59,1.964 2.765,9.21 3.246,10.758c0.3,0.965 0.789,2.233 1.646,2.494c0.752,0.29 1.5,0.025 1.984,-0.355l5.437,-5.043l8.777,6.845l0.209,0.125c0.596,0.264 1.167,0.396 1.712,0.396c0.421,0 0.825,-0.079 1.211,-0.237c1.315,-0.54 1.841,-1.793 1.896,-1.935l6.556,-34.077c0.4,-1.82 -0.156,-2.746 -0.694,-3.201zM22,32l-3,8l-3,-10l23,-17z"></path></g></g>
+
   </svg>
 </template>
 

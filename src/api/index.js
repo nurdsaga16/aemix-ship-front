35d6b17b6d10fdb57c6ce6ad92b2adb46888b67a
nurdsaga16 <nurd.saga14@gmail.com>
2026-02-07 17:18:27 +0500

    changed url of api

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: 'https://aemix-ship-back-1-production.up.railway.app/api',
+  baseURL: 'https://aemix-ship-back-1.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
     Accept: 'application/json',
